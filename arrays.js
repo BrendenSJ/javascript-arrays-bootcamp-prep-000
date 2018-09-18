3b2799a738bf09aa6704ee2087cf4b661d3edf06
Brenden SJ <sj50179@gmail.com>
2018-09-18 17:19:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,29 +1,31 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray() {
-  
+  chocolateBars = ["mars", ...chocolateBars]
+  return chocolateBars
 }
 
 function destructivelyAddElementToBeginningOfArray() {
-  
+  chocolateBars.unshift("mars")
+  return chocolateBars
 }
 
 function accessElementInArray() {
-  
+
 }
 
 function destructivelyRemoveElementFromBeginningOfArray() {
-  
+
 }
 
 function removeElementFromBeginningOfArray() {
-  
+
 }
 
 function destructivelyRemoveElementFromEndOfArray() {
-  
+
 }
 
 function removeElementFromEndOfArray() {
-  
+
 }
