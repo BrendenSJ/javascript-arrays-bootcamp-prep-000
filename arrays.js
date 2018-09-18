c4854401c86c0d37cce7c150105fdd3fad82ba14
Brenden SJ <sj50179@gmail.com>
2018-09-18 17:23:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,16 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
+function addElementToEndOfArray(array, element) {
+  array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
 function accessElementInArray() {
 
 }
